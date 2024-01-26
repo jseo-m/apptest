ffd8d36e668049cb46378233f7d623c17a2dcaa5
jseo-m <mon6916@gmail.com>
2024-01-26 17:45:12 +0900

    test

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.js to start working on your app!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+  return(
+    <>
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+        <WebView 
+          source={{ uri: 'https://msumer.com' }} 
+          allowsBackForwardNavigationGestures={true}
+          style={{ marginTop: 0 }} 
+        />
+      </SafeAreaView>
+      </>
+     )
 }
 
 const styles = StyleSheet.create({
